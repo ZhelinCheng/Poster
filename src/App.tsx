036a5ccaaa4c56f4416c453cdcb51a7a225e2e0a
ZhelinCheng <chengzhelin@foxmail.com>
2022-09-23 20:38:51 +0800

    feat: 初始化

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author       : 程哲林
+ * @Date         : 2022-09-23 20:07:29
+ * @LastEditors  : 程哲林
+ * @LastEditTime : 2022-09-23 20:36:12
+ * @FilePath     : /Poster/src/App.tsx
+ * @Description  : 未添加文件描述
+ */
+import React from 'react';
+import styles from './App.module.scss';
+
+console.log(window.fabric)
+
+function App() {
+  return <div className={styles.main}>111</div>;
+}
+
+export default App;
